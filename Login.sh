95e861615a03a1594ac88e7a3363d6bf84e1c7e5
Andy <mastamage1218@gmail.com>
2013-11-13 12:00:33 -0500

    message

---
+++ Login.sh
@@ -0,0 +1,2 @@
+ssh a30024005@hpc-frontend.mcla.edu
+mysql -u BioInfoClub -p
---
+++ Update.sh
@@ -0,0 +1,3 @@
+git add . #Add any new files in your directory to local reposit
+git commit -a -m "message" #Commit all changes to reposit
+git push origin master #Push local reposit
