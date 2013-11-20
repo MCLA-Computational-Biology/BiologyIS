541bbf91b15a48fb8b48a180d4738a8b1b0cfbfe
Andy <mastamage1218@gmail.com>
2013-11-20 12:25:35 -0500

    message

--- Update.sh
+++ Update.sh
@@ -1,3 +1,4 @@
+git pull -m "message" #pull everything so I don't overwrite a new change
 git add -a . #Add any new files in your directory to local reposit
 git commit -a -m "message" #Commit all changes to reposit
 #Change message to be updated information
