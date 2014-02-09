49201d8eee72d0a4f9b0e8d1792fbc971aeecc82
Kurtiss Keefner <kkeefner@gmail.com>
2014-02-09 16:08:32 -0500

    some sql changes

---
+++ Web/sqlerror.php
@@ -0,0 +1,29 @@
+<?php
+class SQLError
+{
+	private $m;
+	private $e = "";
+
+	function __construct($map, $error)
+	{
+		this->m[] = $map;
+		this->error = $e;
+	}
+	
+	function __construct($error)
+	{
+		this->$m = NULL;
+		this->$e = $error
+	}
+	function getmap()
+	{
+		return $this->m;
+	}
+	
+	function geterror()
+	{
+		return $this->e;
+	}
+
+	
+?>
